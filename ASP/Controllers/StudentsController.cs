0725a9f46ffb8bac4b3f4524c551d0755871c043
Bogdan <Bogdan@DESKTOP-TN1VCHG>
2020-01-20 22:33:26 +0200

    Add project files.

---
+++ ASP/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASP.Models;
+using System.Web.Mvc;
+
+namespace ASP.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly StudentData studentData;
+
+        public StudentsController()
+        {
+            this.studentData = new StudentData();
+        }
+
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            List<Student> students = this.studentData.GetStudents();
+            return View(students);
+        }
+
+        public ActionResult ById()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public  ActionResult ById(string id)
+        {
+            var student = this.studentData.GetStudent(Convert.ToInt32(id));
+            return View(student);
+        }
+    }
+}
---
+++ ASP/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+
+    }
+
+    public class StudentData
+    {
+        private static readonly List<Student> StudList = new List<Student>();
+        static StudentData()
+        {
+            StudList.Add(new Student { Id = 1, Name = "Nume1", Faculty = "AC" });
+            StudList.Add(new Student { Id = 2, Name = "Nume2", Faculty = "ETTI" });
+            StudList.Add(new Student { Id = 3, Name = "Nume3", Faculty = "Info" });
+            StudList.Add(new Student { Id = 4, Name = "Nume4", Faculty = "Mate" });
+            StudList.Add(new Student { Id = 5, Name = "Nume5", Faculty = "MC" });
+        }
+        public StudentData()
+        { }
+
+
+        public List<Student> GetStudents()
+        {
+            return StudList;
+        }
+
+        public Student GetStudent(int id)
+        {
+            return StudList.Find(x => x.Id == id);
+        }
+    }
+}
